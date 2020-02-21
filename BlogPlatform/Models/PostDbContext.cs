5b23a4278db026df1c7853c2919bea3e2c19f4a9
ghassan <gh09an@gmail.com>
2020-02-21 04:44:53 -0500

    Added models and controllers

--- BlogPlatform/Models/PostDbContext.cs
+++ BlogPlatform/Models/PostDbContext.cs
@@ -28,65 +28,65 @@ namespace BlogPlatform.Models
             base.OnConfiguring(optionsBuilder);
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<ProductModel>().HasData(
-                new ProductModel
-                {
-                    Id = 1,
-                    Name = "Pac-Man",
-                    Description = "Yellow fruit-eating monster",
-                    Review = "I bought this arcade game, now I hate ghosts!",
-                    Image = "/img/pacman.jpg" ,
-                    ReviewId = 1
-                },
-                new ProductModel
-                {
-                    Id = 2,
-                    Name = "Gauntlet Legends",
-                    Description = "Save the Realms from certain doom",
-                    Review = "Wtf I love this game?",
-                    Image = "/img/GauntletLegends.jpg",
-                     ReviewId = 1
-                },
-                new ProductModel
-                {
-                    Id = 3,
-                    Name = "DigDug",
-                    Description = "Dig and dug",
-                    Review = "Being inflated till you explode has got to hurt!",
-                    Image = "/img/digdug.jpg" ,
-                    ReviewId = 3
-                }
-                );
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<ProductModel>().HasData(
+        //        new ProductModel
+        //        {
+        //            Id = 1,
+        //            Name = "Pac-Man",
+        //            Description = "Yellow fruit-eating monster",
+        //            Review = "I bought this arcade game, now I hate ghosts!",
+        //            Image = "/img/pacman.jpg" ,
+        //            ReviewId = 1
+        //        },
+        //        new ProductModel
+        //        {
+        //            Id = 2,
+        //            Name = "Gauntlet Legends",
+        //            Description = "Save the Realms from certain doom",
+        //            Review = "Wtf I love this game?",
+        //            Image = "/img/GauntletLegends.jpg",
+        //             ReviewId = 1
+        //        },
+        //        new ProductModel
+        //        {
+        //            Id = 3,
+        //            Name = "DigDug",
+        //            Description = "Dig and dug",
+        //            Review = "Being inflated till you explode has got to hurt!",
+        //            Image = "/img/digdug.jpg" ,
+        //            ReviewId = 3
+        //        }
+        //        );
 
 
 
-            modelBuilder.Entity<Review>().HasData(
-                new Review()
-                {
-                    Id = 1,
-                    Description = "hlkjhlkjhkjlh",
-                    Name = "ghassan"
-                },
-                  new Review()
-                  {
-                      Id = 2,
-                      Description = "hlkjhlkjhkjlh",
-                    Name = "ghassan"
-                  },
+        //    modelBuilder.Entity<Review>().HasData(
+        //        new Review()
+        //        {
+        //            Id = 1,
+        //            Description = "hlkjhlkjhkjlh",
+        //            Name = "ghassan"
+        //        },
+        //          new Review()
+        //          {
+        //              Id = 2,
+        //              Description = "hlkjhlkjhkjlh",
+        //            Name = "ghassan"
+        //          },
 
-                new Review()
-                {
-                    Id = 3,
-                    Description = "nbgkljglkjghlkjhl",
-                     Name = "ali"
-                });
+        //        new Review()
+        //        {
+        //            Id = 3,
+        //            Description = "nbgkljglkjghlkjhl",
+        //             Name = "ali"
+        //        });
 
-            base.OnModelCreating(modelBuilder);
+        //    base.OnModelCreating(modelBuilder);
 
-            //  base.OnConfiguring(modelBuilder);
-        }
+        //    //  base.OnConfiguring(modelBuilder);
+        //}
     }
 }
 
